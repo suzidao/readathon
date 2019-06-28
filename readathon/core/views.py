f7504d66b0d1b1f490fba88c413c8226b0d6f8f0
suzidao <suzi.dao@gmail.com>
2019-06-28 16:17:26 -0400

    populate all indices and link them to each other

--- readathon/core/views.py
+++ readathon/core/views.py
@@ -5,5 +5,6 @@ from readathon.books.models import Book
 
 def index(request):
   participants = Reader.objects.order_by('name')
-  context = {'participants': participants}
+  library = Book.objects.order_by('title')
+  context = {'participants': participants, 'library': library}
   return render(request, 'index.html', context)
